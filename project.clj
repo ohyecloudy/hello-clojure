06d0ea45cf655533381c53c97bca454b76c36438
ohyecloudy <ohyecloudy@gmail.com>
2013-03-29 23:23:53 +0900

    leiningen > project.clj 추가 * update gitignore

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject hello-clojure "0.1.0-SNAPSHOT"
+  :description "clojure 학습 프로젝트"
+  :url "https://github.com/ohyecloudy/hello-clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
