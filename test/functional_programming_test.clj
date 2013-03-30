36ad8a0324de3607dd1c6344e3f7df372be39d9d
ohyecloudy <ohyecloudy@gmail.com>
2013-03-30 22:26:07 +0900

    functional programming

--- test/functional_programming_test.clj
+++ test/functional_programming_test.clj
@@ -63,3 +63,102 @@
 (is (= 34 (nth (fibo) 9))
     "시퀀스 라이브러리를 사용")
 
+; 컬렉션의 헤드를 참조하는 코드 (이렇게 하지 말 것!)
+; (def head-fibo
+;   (lazy-cat [0M 1M]
+;             (map + head-fibo (rest head-fibo))))
+; (nth head-fibo 1000000M)
+;
+; 최상위 var인 head-fibo가 컬렉션의 '헤드를 참조'
+; 피보나치 수열을 만드는 데 사용한 앞쪽 원소를 
+; 가비지 콜렉터가 수거하는 것이 불가능
+;
+; java.lang.OutOfMemoryError 발생. 
+
+(defn count-heads-pairs [coll]
+  (loop [cnt 0 coll coll]
+    (if (empty? coll)
+      cnt
+      (recur (if (= :h (first coll) (second coll))
+               (inc cnt)
+               cnt)
+             (rest coll)))))
+(is (= 2 (count-heads-pairs [:h :h :h :t :h])))
+; '연속 두 번 등장한다'는 개념이 분명히 표현X
+; - loop/recur 속에 뒤섞여 있기 때문
+
+; 복잡한 버전
+(defn by-pairs [coll]
+  (let [take-pair (fn [c]
+                    (when (next c) (take 2 c)))]
+    (lazy-seq
+      (when-let [pair (seq (take-pair coll))]
+        (cons pair (by-pairs (rest coll)))))))
+(is (= nil (next [1])))
+(is (= '(2) (next [1 2])))
+
+(is (= '((:h :t) (:t :t) (:t :h) (:h :h) (:h :h))
+       (by-pairs [:h :t :t :h :h :h])))
+
+(defn count-heads-pairs-2 [coll]
+  (count (filter (fn [pair] (every? #(= :h %) pair))
+                 (by-pairs coll))))
+(is (= 2 (count-heads-pairs-2 [:h :t :t :h :h :h])))
+
+; 시퀀스 함수 사용
+(is (= '((:h :t) (:t :h) (:h :h)) 
+       (partition 2 [:h :t :t :h :h :h])))
+(is (= '((:h :t) (:t :t) (:t :h) (:h :h) (:h :h))
+       (partition 2 1 [:h :t :t :h :h :h])))
+(is (= (partition 2 1 [:h :t :t :h :h :h])
+       (by-pairs [:h :t :t :h :h :h]))
+    "구현한 by-pairs는 (partition 2 1 coll)과 같다")
+
+; TODO: 책과 다름. 1.3 이후에는 defvar 대신에 def를 사용
+(def count-if (comp count filter))
+(is (= 3 (count-if odd? [1 2 3 4 5])))
+
+; count-if와 partition을 이용해 보편적인 함수를 정의
+(defn count-runs
+  "Count runs of length n where pred is true in coll."
+  [n pred coll]
+  (count-if #(every? pred %) (partition n 1 coll)))
+
+(is (= (count-heads-pairs-2 [:h :t :t :h :h :h])
+       (count-runs 2 #(= % :h) [:h :t :t :h :h :h])))
+
+(def count-heads-pairs-3 
+  "partial은 함수 f와 함수 인자 목록 중 일부를 받아
+  f를 부분적으로만 적용.
+  나머지 인자들은 partial이 만든 함수에 넘긴다"
+  (partial count-runs 2 #(= % :h)))
+(is (= (count-heads-pairs-2 [:h :t :t :h :h :h])
+       (count-heads-pairs-3 [:h :t :t :h :h :h])))
+
+; (partial count-runs 2 #(= % :h)) 은 
+; (fn [coll] (count-runs 2 #(= % :h) coll))
+; 코드의 좀 더 효과적인 표현
+
+;-------------------------------------------------------------------------------
+; recursion
+
+(declare my-odd? my-even?)
+
+(defn my-odd? [n]
+  (if (= n 0) 
+    false
+    (my-even? (dec n))))
+
+(defn my-even? [n]
+  (if (= n 0)
+    true
+    (my-odd? (dec n))))
+
+(is (= '(true false true false)
+       (map my-even? (range 4))))
+(is (= '(false true false true)
+       (map my-odd? (range 4))))
+
+; StackOverflowError
+; (my-even? 1000000M)
+
